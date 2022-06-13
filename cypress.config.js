2d2c831f08aaf86b204ccced8ccc6839f8e0444d
sarojkarkii <sawrosecarkey@gmail.com>
2022-06-13 12:25:59 +0545

    chromeweb security set as false

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,14 @@ const { defineConfig } = require("cypress");
 const tagify = require('cypress-tags')
 module.exports = defineConfig({
   projectId: 'fx8u8f',
+  chromeWebSecurity: false,
 
   env:{
     email:'saroj.karki@bajratechnologies.com',
     email1:"randommail@gmail.com",
     password:'Savechanges206206',
     name:'Saroj Karki',
-    CYPRESS_RECORD_KEY:'5ac5323d-3710-4d74-b41f-6ee4acdee9f1',
+    //CYPRESS_RECORD_KEY:'5ac5323d-3710-4d74-b41f-6ee4acdee9f1',
  
   },
   e2e: {
